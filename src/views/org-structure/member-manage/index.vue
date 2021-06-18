946e1beefc5d5498161fd6161935f8ba2f4acf52
欧杰烽 <oujiefeng@9kd.com>
2021-06-18 11:03:40 +0800

    成员管理页面修改字段名称

--- src/views/org-structure/member-manage/index.vue
+++ src/views/org-structure/member-manage/index.vue
@@ -20,7 +20,17 @@
         </div>
       </div>
       <div class="department-list" v-show="!showFlag">
-        <el-tree :data="departmentTree" node-key="id" :props="{ value:'id',children:'departmentRespList' }" :expand-on-click-node="true" :highlight-current="true" :current-node-key="-1" :default-expanded-keys="[-1]" :default-checked-keys="[-1]" @node-click="handleNodeClick">
+        <el-tree
+          :data="departmentTree"
+          node-key="id"
+          :props="{ value:'id',children:'departmentRespList' }"
+          :expand-on-click-node="true"
+          :highlight-current="true"
+          :current-node-key="-1"
+          :default-expanded-keys="[-1]"
+          :default-checked-keys="[-1]"
+          @node-click="handleNodeClick"
+        >
           <template slot-scope="{ node, data }">
             <span style="font-size: 16px;text-overflow: ellipsis;white-space: nowrap;overflow: hidden;">{{
                 data.departmentName
@@ -40,7 +50,15 @@
         </div>
       </div>
       <div class="content_item table_wrap">
-        <el-table style="width: 100%" :header-cell-style="{background:'rgba(245,245,245,0.5)',color:'#000',fontSize:'15px'}" ref="multipleTable" :data="memberData" tooltip-effect="dark" @row-click="skipToDetail" @selection-change="handleSelectionChange">
+        <el-table
+          style="width: 100%"
+          :header-cell-style="{background:'rgba(245,245,245,0.5)',color:'#000',fontSize:'15px'}"
+          ref="multipleTable"
+          :data="memberData"
+          tooltip-effect="dark"
+          @row-click="skipToDetail"
+          @selection-change="handleSelectionChange"
+        >
           <el-table-column type="selection" width="55">
           </el-table-column>
           <el-table-column label="姓名/联系人姓名" width="200">
@@ -70,20 +88,28 @@
               {{ scope.row.postName ? scope.row.postName : '-' }}
             </template>
           </el-table-column>
-          <el-table-column width="200" prop="phone" label="手机号" show-overflow-tooltip>
+          <el-table-column width="200" prop="phone" label="手机号/联系人电话" show-overflow-tooltip>
             <template slot-scope="scope">
-              {{ scope.row.phone ? scope.row.phone : '-' }}
-            </template>
-          </el-table-column>
-          <el-table-column width="200" prop="companyPhone" label="联系电话" show-overflow-tooltip>
-            <template slot-scope="scope">
-              {{ scope.row.contactPhone ? scope.row.contactPhone : '-' }}
+              <span v-if="scope.row.type === 0">
+                {{ scope.row.phone ? scope.row.phone : '-' }}
+              </span>
+              <span v-if="scope.row.type === 1">
+                {{ scope.row.contactPhone ? scope.row.contactPhone : '-' }}
+              </span>
             </template>
           </el-table-column>
         </el-table>
       </div>
       <div class="content_item">
-        <el-pagination :hide-on-single-page="false" background @size-change="handleSizeChange" @current-change="handleCurrentChange" :current-page.sync="currentpage" :page-size="pageSize" layout="total, sizes, prev, pager, next, jumper" :total="totalPages">
+        <el-pagination
+          :hide-on-single-page="false"
+          background
+          @size-change="handleSizeChange"
+          @current-change="handleCurrentChange"
+          :current-page.sync="currentpage"
+          :page-size="pageSize"
+          layout="total, sizes, prev, pager, next, jumper"
+          :total="totalPages">
         </el-pagination>
       </div>
     </div>
@@ -94,13 +120,20 @@
         <div class="from_wrap">
           <div class="from_item">
             <div class="from_item_label">选择部门：</div>
-            <el-cascader clearable :show-all-levels="false" :options="departmentOptions" :props="{
+            <el-cascader
+              clearable
+              :show-all-levels="false"
+              :options="departmentOptions"
+              :props="{
                 emitPath:false,
                 checkStrictly: true ,
                 value:'id',
                 label:'departmentName',
                 children:'departmentRespList'
-              }" v-model="departmentCas" placeholder="请选择部门" @change="handlerDepartmentChange">
+              }"
+              v-model="departmentCas"
+              placeholder="请选择部门"
+              @change="handlerDepartmentChange">
             </el-cascader>
           </div>
           <div class="from_item" style="padding-left: 80px">
@@ -116,141 +149,141 @@
 <script src="./member-manage.js"></script>
 
 <style rel="stylesheet/scss" lang="scss" scoped>
-  @import "src/styles/common.scss";
+@import "src/styles/common.scss";
 </style>
 
 <style lang="scss">
-  .menber_manager_container {
-    width: 100%;
-    display: flex;
+.menber_manager_container {
+  width: 100%;
+  display: flex;
 
-    .menu_tree {
-      width: 20%;
+  .menu_tree {
+    width: 20%;
 
-      .search_wrap {
-        padding: 20px;
-        background: rgba(245, 245, 245, 0.5);
-      }
-
-      .department-list {
-        height: calc(100vh - 165px);
-        overflow-y: auto;
-      }
-
-      .search-result {
-        height: calc(100vh - 165px);
-        overflow-y: auto;
-        background: rgba(245, 245, 245, 0.5);
-
-        .member-list {
-          padding: 0 20px;
-
-          .member-item {
-            display: flex;
-            align-items: center;
-            margin-bottom: 10px;
-            padding: 10px 0;
-            cursor: pointer;
-            border-bottom: 1px solid #ebeef5;
-
-            &:hover {
-              background: rgba(245, 245, 245, 0.8);
-            }
-
-            .name {
-              margin-left: 5px;
-            }
-
-            .avatar {
-              width: 60px;
-              height: 60px;
-              border-radius: 50%;
-              background: #fcc;
-              margin-right: 20px;
-            }
-          }
-        }
-      }
-
-      .el-tree {
-        height: calc(100vh - 165px);
-        background: rgba(245, 245, 245, 0.5);
-        font-family: Helvetica Neue, Helvetica, PingFang SC, Hiragino Sans GB,
-          Microsoft YaHei, Arial, sans-serif;
-        font-size: 24px;
-        color: #000;
-
-        .el-tree-node__content {
-          height: 50px;
-        }
-
-        .el-tree-node__expand-icon {
-          font-size: 18px;
-        }
-      }
-
-      .el-tree--highlight-current
-        .el-tree-node.is-current
-        > .el-tree-node__content {
-        background-color: rgba(64, 158, 255, 0.5);
-        color: #fff;
-      }
-    }
-
-    .content_wrap {
-      width: 70%;
+    .search_wrap {
       padding: 20px;
-
-      .name {
-        font-size: 22px;
-        color: #000;
-      }
-
-      .ico {
-        font-size: 18px;
-
-        .el-icon-s-custom {
-          font-size: 18px;
-          margin-right: 5px;
-        }
-      }
-
-      .operation {
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-      }
-
-      .content_item {
-        margin-top: 30px;
-      }
-
-      .table_wrap {
-        height: 530px;
-        overflow-y: auto;
-      }
+      background: rgba(245, 245, 245, 0.5);
     }
 
-    .adjust_dialog {
-      .from_wrap {
-        .from_item {
+    .department-list {
+      height: calc(100vh - 165px);
+      overflow-y: auto;
+    }
+
+    .search-result {
+      height: calc(100vh - 165px);
+      overflow-y: auto;
+      background: rgba(245, 245, 245, 0.5);
+
+      .member-list {
+        padding: 0 20px;
+
+        .member-item {
           display: flex;
           align-items: center;
-          margin-bottom: 40px;
+          margin-bottom: 10px;
+          padding: 10px 0;
+          cursor: pointer;
+          border-bottom: 1px solid #ebeef5;
 
-          .from_item_label {
-            width: 80px;
+          &:hover {
+            background: rgba(245, 245, 245, 0.8);
           }
 
-          .el-input {
-            width: 350px;
+          .name {
+            margin-left: 5px;
+          }
+
+          .avatar {
+            width: 60px;
+            height: 60px;
+            border-radius: 50%;
+            background: #fcc;
+            margin-right: 20px;
           }
         }
       }
+    }
 
-      .el-button {
-        margin-right: 20px;
+    .el-tree {
+      height: calc(100vh - 165px);
+      background: rgba(245, 245, 245, 0.5);
+      font-family: Helvetica Neue, Helvetica, PingFang SC, Hiragino Sans GB,
+      Microsoft YaHei, Arial, sans-serif;
+      font-size: 24px;
+      color: #000;
+
+      .el-tree-node__content {
+        height: 50px;
+      }
+
+      .el-tree-node__expand-icon {
+        font-size: 18px;
       }
     }
+
+    .el-tree--highlight-current
+    .el-tree-node.is-current
+    > .el-tree-node__content {
+      background-color: rgba(64, 158, 255, 0.5);
+      color: #fff;
+    }
   }
+
+  .content_wrap {
+    width: 70%;
+    padding: 20px;
+
+    .name {
+      font-size: 22px;
+      color: #000;
+    }
+
+    .ico {
+      font-size: 18px;
+
+      .el-icon-s-custom {
+        font-size: 18px;
+        margin-right: 5px;
+      }
+    }
+
+    .operation {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+    }
+
+    .content_item {
+      margin-top: 30px;
+    }
+
+    .table_wrap {
+      height: 530px;
+      overflow-y: auto;
+    }
+  }
+
+  .adjust_dialog {
+    .from_wrap {
+      .from_item {
+        display: flex;
+        align-items: center;
+        margin-bottom: 40px;
+
+        .from_item_label {
+          width: 80px;
+        }
+
+        .el-input {
+          width: 350px;
+        }
+      }
+    }
+
+    .el-button {
+      margin-right: 20px;
+    }
+  }
+}
 </style>
