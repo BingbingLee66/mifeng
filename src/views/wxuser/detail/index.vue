5af834f0ac0cd26c2cc02132b8e9ad9ee04fdba7
欧杰烽 <oujiefeng@9kd.com>
2021-07-12 14:25:41 +0800

    修复bug：总后台-用户管理，用户详情中，用户还显示公司介绍

--- src/views/wxuser/detail/index.vue
+++ src/views/wxuser/detail/index.vue
@@ -6,13 +6,13 @@
       </el-row>
       <el-row>
         <el-col :span="1" class="wx-col col-title">ID</el-col>
-        <el-col :span="2" class="wx-col">{{userDetail.id ? userDetail.id : '空'}}</el-col>
+        <el-col :span="2" class="wx-col">{{ userDetail.id ? userDetail.id : '空' }}</el-col>
         <el-col :span="2" class="wx-col col-title">微信</el-col>
-        <el-col :span="3" class="wx-col">{{userDetail.uname ? userDetail.uname : '未绑定'}}</el-col>
+        <el-col :span="3" class="wx-col">{{ userDetail.uname ? userDetail.uname : '未绑定' }}</el-col>
         <el-col :span="2" class="wx-col col-title">手机号</el-col>
-        <el-col :span="3" class="wx-col">{{userDetail.phone ? userDetail.phone : '空'}}</el-col>
+        <el-col :span="3" class="wx-col">{{ userDetail.phone ? userDetail.phone : '空' }}</el-col>
         <el-col :span="2" class="wx-col col-title">注册时间</el-col>
-        <el-col :span="4" class="wx-col">{{userDetail.createdTs ? userDetail.createdTs : '空'}}</el-col>
+        <el-col :span="4" class="wx-col">{{ userDetail.createdTs ? userDetail.createdTs : '空' }}</el-col>
         <el-col :span="2" class="wx-col col-title">注册方式</el-col>
         <el-col :span="3" class="wx-col">
           <span v-if="userDetail.registerType == 0">商会后台</span>
@@ -24,12 +24,12 @@
     </div>
     <div style="margin-top: 20px;">
       <el-row>
-        <span class="row-title">已加入商会（{{count}}）</span>
+        <span class="row-title">已加入商会（{{ count }}）</span>
       </el-row>
       <table v-for="(member, index) in memberList" border="1" width="100%" align="center" cellspacing="0">
         <tr align="center" height="45">
           <td width="100%" colspan="5">
-            <div style="font-size: 18px; font-weight: 800;">{{member.chamberName}}</div>
+            <div style="font-size: 18px; font-weight: 800;">{{ member.chamberName }}</div>
           </td>
         </tr>
         <tr align="left" height="45">
@@ -39,12 +39,12 @@
         </tr>
         <tr align="center" height="45">
           <td width="24%" rowspan="6">
-            <div class="head-portrait"><img :src="member.portrait" /></div>
+            <div class="head-portrait"><img :src="member.portrait"/></div>
           </td>
           <td width="8%">姓名</td>
-          <td width="30%">{{member.name}}</td>
+          <td width="30%">{{ member.name }}</td>
           <td width="8%">入会时间</td>
-          <td width="30%">{{member.createdTs}}</td>
+          <td width="30%">{{ member.createdTs }}</td>
         </tr>
         <tr align="center" height="45">
           <td width="8%">性别</td>
@@ -53,28 +53,32 @@
             <div v-if="member.gender == 2">女</div>
           </td>
           <td width="8%">商会职位</td>
-          <td width="30%">{{member.postName}}</td>
+          <td width="30%">{{ member.postName }}</td>
         </tr>
         <tr align="center" height="45">
           <td width="8%">籍贯</td>
-          <td width="30%">{{member.nativePlace}}</td>
+          <td width="30%">{{ member.nativePlace }}</td>
           <td width="8%">身份证号码</td>
-          <td width="30%">{{member.idCard}}</td>
+          <td width="30%">{{ member.idCard }}</td>
         </tr>
         <tr align="center" height="45">
           <td width="8%">生日</td>
-          <td width="30%">{{member.birthday}}</td>
+          <td width="30%">{{ member.birthday }}</td>
           <td width="8%">身份证照</td>
-          <td width="30%"><el-button type="text" @click="idCardDetail(member)">详情</el-button></td>
+          <td width="30%">
+            <el-button type="text" @click="idCardDetail(member)">详情</el-button>
+          </td>
         </tr>
         <tr align="center" height="45">
           <td width="8%">手机号码</td>
-          <td colspan="1">{{member.phone}}</td>
+          <td colspan="1">{{ member.phone }}</td>
           <td colspan="2"></td>
         </tr>
         <tr align="center" height="45">
           <td width="8%">个人履历</td>
-          <td align="left" height="150" colspan="10">{{resumeCp(member.resume)}} <el-button type="text" @click="resumeDetail(member.resume)">详情</el-button></td>
+          <td align="left" height="150" colspan="10">{{ resumeCp(member.resume) }}
+            <el-button type="text" @click="resumeDetail(member.resume)">详情</el-button>
+          </td>
         </tr>
         <tr align="left" height="45">
           <td width="100%" colspan="5">
@@ -83,28 +87,37 @@
         </tr>
         <tr align="center" height="45">
           <td width="24%" rowspan="4">
-            <div class="head-portrait"><img :src="member.companyLogo" /></div>
+            <div class="head-portrait"><img :src="member.companyLogo"/></div>
           </td>
           <td width="8%">公司名称</td>
-          <td width="30%">{{member.companyName}}</td>
+          <td width="30%">{{ member.companyName }}</td>
           <td width="8%">职务</td>
-          <td width="30%">{{member.companyPosition}}</td>
+          <td width="30%">{{ member.companyPosition }}</td>
         </tr>
         <tr align="center" height="45">
-          <td width="8%">行业</td>
-          <td width="30%">{{member.tradeName}}</td>
-          <td width="8%">联系电话</td>
-          <td width="30%">{{member.companyPhone}}</td>
+          <template v-if="member.type===1">
+            <td width="8%">联系人姓名</td>
+            <td width="30%">{{ member.contactName }}</td>
+          </template>
+          <template v-else>
+            <td width="8%">行业</td>
+            <td width="30%">{{ member.tradeName }}</td>
+          </template>
+          <td width="8%">联系人电话</td>
+          <td width="30%">{{ member.contactPhone }}</td>
         </tr>
         <tr align="center" height="45">
           <td width="8%">办公地址</td>
-          <td width="30%">{{member.companyAddress}}</td>
+          <td width="30%">{{ member.companyAddress }}</td>
           <td width="8%">营业执照</td>
-          <td width="30%"><el-button type="text" @click="licenseDetail(member)">详情</el-button></td>
+          <td width="30%">
+            <el-button type="text" @click="licenseDetail(member)">详情</el-button>
+          </td>
         </tr>
-        <tr align="center" height="45">
-          <td width="8%">公司介绍</td>
-          <td align="left" height="150" colspan="10">{{companyInstrodCp(member.companyIntroduction)}} <el-button type="text" @click="companyIntroductionDetail(member.companyIntroduction)">详情</el-button></td>
+        <tr align="center" height="45" v-if="member.type===1">
+          <td width="8%">行业</td>
+          <td align="left" height="150" colspan="10">{{ member.tradeName }}
+          </td>
         </tr>
       </table>
     </div>
@@ -133,7 +146,9 @@
       title="个人履历"
       :visible.sync="resumeVisible"
       width="50%">
-      <el-row><div class="text-detail">{{resume}}</div></el-row>
+      <el-row>
+        <div class="text-detail">{{ resume }}</div>
+      </el-row>
       <span slot="footer" class="dialog-footer">
         <el-button type="primary" @click.native="resumeVisible = false">确定</el-button>
       </span>
@@ -142,7 +157,9 @@
       title="公司介绍"
       :visible.sync="companyIntroductionVisible"
       width="50%">
-      <el-row><div class="text-detail">{{companyIntroduction}}</div></el-row>
+      <el-row>
+        <div class="text-detail">{{ companyIntroduction }}</div>
+      </el-row>
       <span slot="footer" class="dialog-footer">
         <el-button type="primary" @click.native="companyIntroductionVisible = false">确定</el-button>
       </span>
@@ -154,46 +171,50 @@
 <script src="./detail.js"></script>
 
 <style rel="stylesheet/scss" lang="scss" scoped>
-  @import "src/styles/common.scss";
+@import "src/styles/common.scss";
 </style>
 <style>
-  .wx-col {
-    border: 1px solid black;
-    border-right: 0;
-    border-radius: 0 !important;
-    line-height: 40px;
-    text-align: center;
-  }
-  .wx-col:last-child {
-    border: 1px solid black;
-  }
-  .col-title {
-    background-color: #b7f0ff;
-  }
-  .row-title {
-    font-size: 20px;
-    font-weight: 800;
-  }
-  .head-portrait {
-    position: relative;
-    width: 80%;
-    height: 0;
-    margin: 0 auto;
-    padding-top: 80%;
-    border: 1px solid black;
-    border-radius: 50%;
-  }
-  .head-portrait>img {
-    position: absolute;
-    height: 100%;
-    width: 100%;
-    border-radius: 50%;
-    top: 0;
-    left: 0;
-  }
-  .text-detail {
-    margin: 0 5%;
-    line-height: 1.8;
-    font-size: 15px;
-  }
+.wx-col {
+  border: 1px solid black;
+  border-right: 0;
+  border-radius: 0 !important;
+  line-height: 40px;
+  text-align: center;
+}
+
+.wx-col:last-child {
+  border: 1px solid black;
+}
+
+.col-title {
+  background-color: #b7f0ff;
+}
+
+.row-title {
+  font-size: 20px;
+  font-weight: 800;
+}
+
+.head-portrait {
+  position: relative;
+  width: 150px;
+  height: 150px;
+  margin: 20px auto;
+  border-radius: 50%;
+}
+
+.head-portrait > img {
+  position: absolute;
+  height: 100%;
+  width: 100%;
+  border-radius: 50%;
+  top: 0;
+  left: 0;
+}
+
+.text-detail {
+  margin: 0 5%;
+  line-height: 1.8;
+  font-size: 15px;
+}
 </style>
