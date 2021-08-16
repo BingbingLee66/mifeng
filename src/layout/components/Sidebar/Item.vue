8f26a242ec7e7d8a7af25cab4f24f83ac747b158
xiaokeai <549426210@qq.com>
2021-08-16 16:43:29 +0800

    修改icon

--- src/layout/components/Sidebar/Item.vue
+++ src/layout/components/Sidebar/Item.vue
@@ -15,9 +15,9 @@ export default {
   render(h, context) {
     const { icon, title } = context.props
     const vnodes = []
-
     if (icon) {
       vnodes.push(<svg-icon icon-class={icon}/>)
+      // vnodes.push(<svg-icon icon-class='icon-password'/>)
     }
 
     if (title) {
