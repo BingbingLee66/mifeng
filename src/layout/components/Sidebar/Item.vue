34d5cffc2658982b8da46275ad4124e6ce43af03
xiaokeai <549426210@qq.com>
2021-08-17 17:27:40 +0800

    间距样式

--- src/layout/components/Sidebar/Item.vue
+++ src/layout/components/Sidebar/Item.vue
@@ -15,7 +15,7 @@ export default {
   render(h, context) {
     const { icon, title } = context.props
     const vnodes = []
-    if (icon) {
+    if (icon && icon !== 'operation') {
       vnodes.push(<svg-icon width='50' height='50' icon-class={icon}/>)
       // vnodes.push(<svg-icon icon-class='icon-password'/>)
     }
--- src/layout/components/Sidebar/SidebarItem.vue
+++ src/layout/components/Sidebar/SidebarItem.vue
@@ -2,8 +2,8 @@
   <div v-if="!item.hidden" class="menu-wrapper">
     <template v-if="hasOneShowingChild(item.children,item) && (!onlyOneChild.children||onlyOneChild.noShowingChildren)&&!item.alwaysShow">
       <app-link v-if="onlyOneChild.meta" :to="resolvePath(onlyOneChild.path)">
-        <el-menu-item :index="resolvePath(onlyOneChild.path)" :class="{'submenu-title-noDropdown':!isNest}">
-
+        <el-menu-item :style="{ paddingLeft: item.path==='/' ? '20px':'60px'}" :index="resolvePath(onlyOneChild.path)" :class="{'submenu-title-noDropdown':!isNest}">
+          <!-- {{ item.path }} -->
           <item :icon="onlyOneChild.meta.icon||(item.meta&&item.meta.icon)" :title="generateTitle(onlyOneChild.meta.title)" />
         </el-menu-item>
       </app-link>
