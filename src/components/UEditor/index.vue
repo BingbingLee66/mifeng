bc845d123ab07f3f31e94070e0e23baa3cf49102
欧杰烽 <oujiefeng@9kd.com>
2021-10-11 10:49:58 +0800

    UEditor工具栏自定义上传图片按钮

--- src/components/UEditor/index.vue
+++ src/components/UEditor/index.vue
@@ -1,6 +1,5 @@
 <template>
   <vue-ueditor-wrap ref="ued" v-model="content" :config="config" @ready="ready" :destroy="true" @beforeInit="addCustomButtom"></vue-ueditor-wrap>
-
 </template>
 <script>
 export default {
@@ -9,6 +8,18 @@ export default {
       content: '',
       editor: {}, // ueditor实例
       config: {
+        // 编辑器不自动被内容撑高
+        autoHeightEnabled: false,
+        // 初始容器高度
+        initialFrameHeight: 500,
+        // 初始容器宽度
+        initialFrameWidth: '100%',
+        // 上传文件接口（这个地址是我为了方便各位体验文件上传功能搭建的临时接口，请勿在生产环境使用！！！）
+        serverUrl:
+          'https://tysh.9kd.com/ecservice/upload/richhtml-custom-img-upload',
+        // UEditor 资源文件的存放路径，如果你使用的是 vue-cli 生成的项目，通常不需要设置该选项，vue-ueditor-wrap 会自动处理常见的情况，如果需要特殊配置，参考下方的常见问题2
+        UEDITOR_HOME_URL: '/UEditor/',
+        // 设置工具栏功能
         toolbars: [
           [
             'undo',
@@ -17,7 +28,7 @@ export default {
             'bold',
             'italic',
             'underline',
-            'forecolor',
+            '|',
             'justifyleft',
             'justifycenter',
             'justifyright',
@@ -30,6 +41,7 @@ export default {
             'paragraph',
             'fontfamily',
             'fontsize',
+            'forecolor',
             'insertorderedlist',
             'insertunorderedlist',
             '|',
@@ -47,31 +59,22 @@ export default {
             // 'insertimage',
           ],
         ],
-        // 编辑器不自动被内容撑高
-        autoHeightEnabled: false,
-        // 初始容器高度
-        initialFrameHeight: 500,
-        // 初始容器宽度
-        initialFrameWidth: '100%',
-        // 上传文件接口（这个地址是我为了方便各位体验文件上传功能搭建的临时接口，请勿在生产环境使用！！！）
-        serverUrl:
-          'https://tysh.9kd.com/ecservice/upload/richhtml-custom-img-upload',
-        // UEditor 资源文件的存放路径，如果你使用的是 vue-cli 生成的项目，通常不需要设置该选项，vue-ueditor-wrap 会自动处理常见的情况，如果需要特殊配置，参考下方的常见问题2
-        UEDITOR_HOME_URL: '/UEditor/',
+
       },
     }
   },
-  mounted() {},
+  mounted() {
+  },
   methods: {
     // 自定义上传图片按钮
     addCustomButtom(editorId) {
       var that = this
       window.UE.registerUI(
         'test-button',
-        function (editor, uiName) {
+        function(editor, uiName) {
           // 注册按钮执行时的 command 命令，使用命令默认就会带有回退操作
           editor.registerCommand(uiName, {
-            execCommand: function () {
+            execCommand: function() {
               editor.execCommand('inserthtml', ``)
             },
           })
@@ -84,7 +87,7 @@ export default {
             // 需要添加的额外样式，可指定 icon 图标
             cssRules: 'background-position: -380px 0px;',
             // 点击时执行的命令
-            onclick: function () {
+            onclick: function() {
               // 此处定义点击按钮时的行为
               // 实现选择的图片上传到服务器，然后将返回的内容添加到编辑器里
               var $file = document.createElement('input')
@@ -92,7 +95,7 @@ export default {
               $file.accept = 'image/*'
               $file.multiple = 'multiple'
               $file.click()
-              $file.addEventListener('change', function (e) {
+              $file.addEventListener('change', function(e) {
                 var fileList = e.target.files
                 if (fileList.length > 0) {
                   let formData = new FormData()
@@ -108,12 +111,12 @@ export default {
                     localStorage.getItem('token')
                   )
                   xhr.send(formData)
-                  xhr.onreadystatechange = function () {
+                  xhr.onreadystatechange = function() {
                     if (xhr.readyState == 4 && xhr.status == 200) {
                       let result = xhr.responseText
                       if (typeof result == 'string') {
                         let res = JSON.parse(result)
-                        if (res.state == 1) {
+                        if (res.state === 1) {
                           // 返回的内容添加到编辑器里
                           editor.execCommand(
                             'inserthtml',
@@ -130,7 +133,7 @@ export default {
             },
           })
           // 当点到编辑内容上时，按钮要做的状态反射
-          editor.addListener('selectionchange', function () {
+          editor.addListener('selectionchange', function() {
             // var state = editor.queryCommandState(uiName);
             // if (state === -1) {
             //   btn.setDisabled(true);
@@ -142,9 +145,8 @@ export default {
           })
           return btn
         },
-        55 /* 指定添加到工具栏上的哪个位置，默认时追加到最后 */,
-        editorId /* 指定这个 UI 是哪个编辑器实例上的，默认是页面上所有的编辑器都会添加这个按钮 */
-      )
+        12 /* 指定添加到工具栏上的哪个位置，默认时追加到最后 */,
+        editorId /* 指定这个 UI 是哪个编辑器实例上的，默认是页面上所有的编辑器都会添加这个按钮 */)
     },
     ready(instance) {
       this.editor = instance // 保存vue-ueditor-wrap实例
