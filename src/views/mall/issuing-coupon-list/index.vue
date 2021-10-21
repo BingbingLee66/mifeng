26bcf6567d92fad4b2bf119c96275bbd9d720803
欧杰烽 <oujiefeng@9kd.com>
2021-10-21 11:30:12 +0800

    已发放的劵发送结果bug

--- src/views/mall/issuing-coupon-list/index.vue
+++ src/views/mall/issuing-coupon-list/index.vue
@@ -63,21 +63,21 @@
                   }}人次</span>无法收到足额的劵
                 </div>
                 <div v-if="item.resultStatus===4">
-                  由于优惠券【<span v-for="(item,index) in scope.row.couponList" :key="index">
-                  {{ item.couponId }}<i v-if="scope.row.couponList.length!==index+1">，</i></span>】被停发，
+                  由于优惠券【<span v-for="(i,index) in item.templateIds" :key="index">
+                  {{ i }}<i v-if="item.templateIds.length!==index+1">，</i></span>】被停发，
                   有<span class="blue-label" @click="showResultDialog(item.phoneStr)">{{ item.phoneStr.length }}人次</span>无法收到足额的券
                 </div>
                 <div v-if="item.resultStatus===3">
-                  由于优惠券【<span v-for="(item,index) in scope.row.couponList" :key="index">{{
-                    item.couponId
-                  }}<i v-if="scope.row.couponList.length!==index+1">，</i></span>】的发行量不够，有<span class="blue-label" @click="showResultDialog(item.phoneStr)">{{
+                  由于优惠券【<span v-for="(i,index) in item.templateIds" :key="index">{{
+                    i
+                  }}<i v-if="item.templateIds.length!==index+1">，</i></span>】的发行量不够，有<span class="blue-label" @click="showResultDialog(item.phoneStr)">{{
                     item.phoneStr.length
                   }}人次</span>无法收到足额的券！
                 </div>
                 <div v-if="item.resultStatus===2">
-                  由于优惠券【<span v-for="(item,index) in scope.row.couponList" :key="index">{{
-                    item.couponId
-                  }}<i v-if="scope.row.couponList.length!==index+1">，</i></span>】的限领限制，有<span class="blue-label" @click="showResultDialog(item.phoneStr)">{{
+                  由于优惠券【<span v-for="(i,index) in item.templateIds" :key="index">{{
+                    i
+                  }}<i v-if="item.templateIds.length!==index+1">，</i></span>】的限领限制，有<span class="blue-label" @click="showResultDialog(item.phoneStr)">{{
                     item.phoneStr.length
                   }}人次</span>无法收到足额的券！
                 </div>
