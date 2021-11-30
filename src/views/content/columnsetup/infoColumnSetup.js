92b0e65b2a756aad36a5e4cae093e1f43ddd3cb9
xiaokeai(#^.^#)小可爱 <549426210@qq.com>
2021-11-30 11:50:04 +0800

    add:总后台栏目设置

--- src/views/content/columnsetup/infoColumnSetup.js
+++ src/views/content/columnsetup/infoColumnSetup.js
@@ -1,31 +1,68 @@
-import { getInfoColumnList, save, updateStatus } from '@/api/content/columnsetup'
-
+import { getInfoColumnList, save, updateStatus ,updateColumnLevel} from '@/api/content/columnsetup'
+import
+kdDialog
+from '@/components/common/kdDialog'
 export default {
   data() {
     var checkNumber = (rule, value, callback) => {
-      if (!/^([1-9]\d*)$/.test(value)) {
+      if (!/^([0-9]\d*)$/.test(value)) {
         return callback(new Error('必须是大于0的整数'))
       } else {
         callback() // 必须加上这个，不然一直塞在验证状态
       }
+    };
+    var checkLevel = (rule, value, callback) => {
+      if (!/^([0-9]\d*)$/.test(value)) {
+        return callback(new Error('权重范围：0-999'))
+      } else if (value > 999) {
+        return callback(new Error('权重范围：0-999'))
+      } else {
+        callback()
+      }
     }
     return {
+      checkList:[],
+      //展示路径
+      typeUrl:[],
       visible: false,
       list: [],
       listLoading: false,
-      formObj: {},
+      formObj: {
+        typeUrl:['1']
+      },
+      
       rules: {
         columnName: [
           { required: true, message: '栏目名称不能为空', trigger: 'blur' }
         ],
         level: [
           { required: true, message: '排序不能为空', trigger: 'blur' },
-          { validator: checkNumber, trigger: 'change' }
+          { validator: checkNumber, trigger: 'blur' }
         ]
+      },
+       //权重rule
+       levelRules: {
+        level: [{
+            required: true,
+            message: '权重不能为空',
+            trigger: 'blur'
+          },
+          {
+            validator: checkLevel,
+            trigger: 'blur'
+          }
+        ],
+      },
+      //权重对象
+      levelForm:{
+        level:""
       }
     }
   },
   computed: {
+  },
+  components: {
+    'kd-dialog': kdDialog
   },
   created() {
     this.init()
@@ -85,8 +122,19 @@ export default {
     save() {
       this.$refs['form'].validate((valid) => {
         if (valid) {
-          console.log('formObj', this.formObj)
-          save(this.formObj).then(response => {
+          console.log('formObj', this.formObj);
+          if(this.typeUrl.length<1){
+            this.$message({
+              message: '请填写展示路径',
+              type: 'error'
+            })
+            return;
+          }
+          let obj={
+            ...this.formObj,
+            typeUrl:this.typeUrl.length>1 ? 3 :this.typeUrl[0]
+          }
+          save(obj).then(response => {
             this.$message({
               message: '操作成功',
               type: 'success'
@@ -98,6 +146,36 @@ export default {
           return false
         }
       })
+    },
+    //设置权重
+    setLevel(row) {
+      this.currentId = row.id;
+      this.$refs['levelDialog'].show()
+    },
+    //保存权重数据
+    savePopupData(){
+      this.$refs['levelForm'].validate((valid) => {
+        if (valid) {
+          //发请求
+          updateColumnLevel({
+            id: this.currentId,
+            level: this.levelForm.level
+          }).then(res => {
+            if (res.state === 1) {
+              this.$message({
+                message: res.msg,
+                type: 'success'
+              })
+              this.init()
+            }
+          })
+          //操作
+          this.$refs['levelDialog'].hide();
+          this.$refs['levelForm'].resetFields();
+        } else {
+          return false;
+        }
+      });
     }
   }
 }
