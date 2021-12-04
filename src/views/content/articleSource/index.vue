aeea343b333e750ce6a52bd120ef6e144df5e23e
张鹏 <zhangpeng@9kd.com>
2021-12-04 17:29:33 +0800

    change: 将总后台》内容管理》编辑台》文章来源管理页面功能实现

--- src/views/content/articleSource/index.vue
+++ src/views/content/articleSource/index.vue
@@ -1,9 +1,37 @@
 <template>
   <div class="app-container">
-    <el-row>
-      <!-- <el-button type="primary" @click="add" :actionid="getId('', '添加来源')" v-if="has('', '添加来源')">添加来源</el-button> -->
-      <el-button type="primary" @click="add">添加来源</el-button>
-    </el-row>
+    <el-form ref="query" label-width="auto" label-position="right" :model="query">
+      <el-row>
+        <el-col :span="5">
+          <el-form-item label="文章来源：">
+            <el-input v-model="query.source" placeholder="关键词" />
+          </el-form-item>
+        </el-col>
+        <el-col :span="3">
+          <el-form-item :span="3" label="状态：">
+            <el-select v-model="query.status">
+              <el-option label="全部" :value="-1" />
+              <el-option label="正常" :value="1" />
+              <el-option label="冻结" :value="0" />
+            </el-select>
+          </el-form-item>
+        </el-col>
+        <el-col :span="3">
+          <el-form-item label="创建人：">
+            <el-input v-model="query.creator" placeholder="关键词" />
+          </el-form-item>
+        </el-col>
+        <el-col :span="1">
+          <el-form-item label=" ">
+            <el-button  type="primary"  @click="queryData()">查询</el-button>
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-row>
+        <!-- <el-button type="primary" @click="add" :actionid="getId('', '添加来源')" v-if="has('', '添加来源')">添加来源</el-button> -->
+        <el-button type="primary" @click="add">添加来源</el-button>
+      </el-row>
+    </el-form>
     <el-table id="out-table" :data="list" v-loading="listLoading" element-loading-text="Loading" border fit highlight-current-row>
       <!-- <el-table-column type="index" label="序号" width="60px">
       </el-table-column> -->
@@ -23,27 +51,41 @@
           <div v-if="scope.row.status == 1">正常</div>
         </template>
       </el-table-column>
-      <el-table-column label="更新人">
+      <el-table-column label="创建信息">
         <template slot-scope="scope">
-          {{scope.row.operator}}
-        </template>
-      </el-table-column>
-      <el-table-column label="更新时间">
-        <template slot-scope="scope">
-          {{scope.row.updatedTs}}
+          <div>
+            {{scope.row.operator}}
+          </div>
+          <div>
+            {{scope.row.updatedTs}}
+          </div>
         </template>
       </el-table-column>
       <el-table-column label="操作" >
         <template slot-scope="scope">
           <!-- <el-button type="text" @click="openVisible(scope.row)" :actionid="getId('', '编辑')" v-if="has('', '编辑')">编辑</el-button> -->
-          <el-button type="text" @click="openVisible(scope.row)">编辑</el-button>
+          <div>
+            <el-button type="text" @click="openVisible(scope.row)">编辑</el-button>
+          </div>
+          <div>
+            <el-button type="text" v-if="scope.row.status == 1" @click="updateStatus(scope.row)">冻结</el-button>
+            <el-button type="text" v-if="scope.row.status == 0" @click="updateStatus(scope.row)">解冻</el-button>
+          </div>
           <!-- <el-button type="text" @click="updateStatus(scope.row)" :actionid="getId('', '冻结')" v-if="has('', '冻结') && scope.row.status == 1">冻结</el-button> -->
-          <el-button type="text" v-if="scope.row.status == 1" @click="updateStatus(scope.row)">冻结</el-button>
           <!-- <el-button type="text" @click="updateStatus(scope.row)" :actionid="getId('', '解冻')" v-if="has('', '解冻') && scope.row.status == 0">解冻</el-button> -->
-          <el-button type="text" v-if="scope.row.status == 0" @click="updateStatus(scope.row)">解冻</el-button>
         </template>
       </el-table-column>
     </el-table>
+    <el-pagination
+      background
+      layout="total, sizes, prev, pager, next, jumper"
+      :page-sizes="pageSizes"
+      :page-size="limit"
+      :total="total"
+      :current-page.sync="currentpage"
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+    />
     <el-dialog
       title="添加/编辑"
       :visible.sync="visible"
