6d66dc1812b6ecd27c95f7c41c07ad63aeadb8fd
xiaokeai(#^.^#)小可爱 <549426210@qq.com>
2021-12-08 12:32:32 +0800

    fix:置顶管理总后台和商会后台调用取消置顶调用接口问题

--- src/views/content/articleupdate/stick/detail.js
+++ src/views/content/articleupdate/stick/detail.js
@@ -28,14 +28,14 @@ export default {
       // 权重rule
       levelRules: {
         level: [{
-          required: true,
-          message: '权重不能为空',
-          trigger: 'blur'
-        },
-        {
-          validator: checkLevel,
-          trigger: 'blur'
-        }
+            required: true,
+            message: '权重不能为空',
+            trigger: 'blur'
+          },
+          {
+            validator: checkLevel,
+            trigger: 'blur'
+          }
         ],
       },
       // 权重对象
@@ -83,9 +83,15 @@ export default {
       this.$refs['levelForm'].validate((valid) => {
         if (valid) {
           // 发请求
-          let interfaceName=this.ckey ? updateChamberContentSort: updateArticleTopLevel;
-          let params=this.ckey ? {id:this.currentId,sort:this.levelForm.level} :{id:this.currentId,level:this.levelForm.level};
-          console.log('interfaceName',interfaceName)
+          let interfaceName = this.ckey ? updateChamberContentSort : updateArticleTopLevel;
+          let params = this.ckey ? {
+            id: this.currentId,
+            sort: this.levelForm.level
+          } : {
+            id: this.currentId,
+            level: this.levelForm.level
+          };
+          console.log('interfaceName', interfaceName)
           interfaceName(params).then(res => {
             if (res.state === 1) {
               this.$message({
@@ -105,12 +111,17 @@ export default {
     // update置顶 这里总后台/商会后台公用 要区分总后台和商会后台调用不同的接口
     updateTop(row) {
       console.log('row', row);
-    // let interfaceName=this.ckey ?  cancelTop
-      if (!this.$store.getters.ckey) {
-        let params = {
-          id: row.id,
-        }
-        cancelTop(params).then(res => {
+      //这里只有取消置顶
+      let interfaceName = this.ckey ? updateChamberTop : cancelTop;
+      let params = this.ckey ? {
+        articleId: row.id,
+        ckey: row.ckey,
+        // 这里统一都是传0
+        type: 0
+      } : {
+        id: row.id
+      };
+      interfaceName(params).then(res => {
           if (res.state === 1) {
             this.$message({
               message: res.msg,
@@ -124,28 +135,7 @@ export default {
           }
           this.fetchData()
         })
-      } else {
-        let params = {
-          articleId: row.id,
-          ckey: row.ckey,
-          // 这里统一都是传0
-          type: 0
-        }
-        updateChamberTop(params).then(res => {
-          if (res.state === 1) {
-            this.$message({
-              message: res.msg,
-              type: 'success'
-            })
-          } else {
-            this.$message({
-              message: res.msg,
-              type: 'error'
-            })
-          }
-          this.fetchData()
-        })
-      }
+      
     }
   }
 }
