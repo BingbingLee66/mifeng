a474a44c9b8dbabfe0e482bba939390ef9993b10
xiaokeai(#^.^#)小可爱 <549426210@qq.com>
2022-05-09 11:38:32 +0800

    add:权重可以输入0

--- src/views/content/kingArea/kingarea.js
+++ src/views/content/kingArea/kingarea.js
@@ -15,7 +15,7 @@ export default {
     //权重表单验证
     var validateWeight = (rule, value, callback) => {
 
-      if(!(/^[1-9]\d*$/.test(value))){
+      if(!(/^[0-9]\d*$/.test(value))){
         callback(new Error('请输入正整数'));
       }else if(value<0 || value>999){
         callback(new Error('权重范围0-999'));
