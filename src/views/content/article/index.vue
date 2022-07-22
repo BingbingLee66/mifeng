d68ab57ddb5c4522f5669d371a4b1603e75b49b0
朱梓杰 <wewewuwu624288>
2022-07-22 18:55:37 +0800

    fix : 【总后台】发布文章很长时，发布按钮防抖没有起到作用

--- src/views/content/article/index.vue
+++ src/views/content/article/index.vue
@@ -318,7 +318,7 @@
       </el-row> -->
       <el-form-item>
         <el-col :span="8">
-          <el-button v-dbClick type="primary" @click="saveFunc">确定</el-button>
+          <el-button v-dbClick type="primary" :loading="requesting" @click="saveFunc">确定</el-button>
           <el-button  @click="closeTab">取消</el-button>
         </el-col>
       </el-form-item>
