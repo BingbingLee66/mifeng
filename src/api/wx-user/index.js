6d2d750ab562a68bdbbb99871894d0e3c642a898
朱梓杰 <zhuzijie@9kd.com>
2022-09-09 19:31:47 +0800

    fix: 接口路径更改

--- src/api/wx-user/index.js
+++ src/api/wx-user/index.js
@@ -1,25 +1,25 @@
-import request from "@/utils/request";
+import request from '@/utils/request'
 
 // 获取加入商会并且未冻结的用户
 export const getJoinChamberAndNoFreezesUsers = params =>
   request({
-    url: "/ec/wx-user/joinChamberAndNoFreezes",
-    method: "get",
+    url: '/ec/wx-user/joinChamberAndNoFreezes',
+    method: 'get',
     params
-  });
+  })
 
 // 总后台入会审核列表
 export const getAllAuditList = params =>
   request({
-    url: "/ec/member/all-audit-list",
-    method: "get",
+    url: '/ec/member/all-audit-list',
+    method: 'get',
     params
-  });
+  })
 
 // 总后台入会审核驳回操作
 export const rejectAuditStatus = data =>
   request({
-    url: "/ec/member/audit-status",
-    method: "put",
+    url: '/ec/member/audit-status',
+    method: 'put',
     data
-  });
+  })
