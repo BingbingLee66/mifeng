a914ba04e36ff6245c24b6f45566bab014145a5e
xiaoqianggang <xiaoqianggang@9kd.com>
2023-01-11 19:44:42 +0800

    feat：添加节日海报类型

--- src/views/mass-notification/util/label.js
+++ src/views/mass-notification/util/label.js
@@ -3,6 +3,7 @@ export const labelType = [
   { n: '缴费通知', type: 1, show: 2 },
   { n: '活动通知', type: 2, show: 3 },
   { n: '课程通知', type: 6, show: 1 },
+  { n: '节日海报', type: 8, show: 1 },
   { n: '招商活动', type: 3, show: 3 },
   { n: '邀请入会', type: 4, show: 2 },
   { n: '自定义通知', type: 5, show: 3 },
@@ -41,7 +42,9 @@ export const labelTypeForm = [
   { name: '招商活动', id: 3 },
   { name: '邀请入会', id: 4 },
   { name: '自定义通知', id: 5 },
-  { name: '课程通知', id: 6 }
+  { name: '课程通知', id: 6 },
+  { name: '节日海报', id: 8 }
+
 ]
 // 所有会员的table config
 export const memberTableConfig = [
