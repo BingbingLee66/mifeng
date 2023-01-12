1bfce77df1123df5767090fd98076c3e1e1bad5d
李志军 <li.zhijun@xlink.cn>
2023-01-12 18:04:05 +0800

    feat: 完成文字链插入

--- src/components/wangEditor/link-menu.js
+++ src/components/wangEditor/link-menu.js
@@ -7,6 +7,7 @@ const { BtnMenu } = E
 // const { $ } = E
 // const { BtnMenu, DropListMenu, PanelMenu, DropList, Panel, Tooltip } = E.menuConstructors
 import Vue from 'vue'
+import { parseUrl } from '@/utils/parseUrl'
 
 // 第一，菜单 class ，Button 菜单继承 BtnMenu class
 class LinkMenu extends BtnMenu {
@@ -21,9 +22,31 @@ class LinkMenu extends BtnMenu {
   }
   // 菜单点击事件
   clickHandler() {
+    const that = this
+    function handleLink(data) {
+      let url = ''
+
+      const urlParams = data.path.split('?')
+      const params = urlParams[1]
+      if (!params) return
+      const { appid, miniProgramType = 0, id } = parseUrl(params)
+
+      if (data.linkType === 'miniprogram') {
+        url = `wechatApp?path=${encodeURIComponent(data.path)}&miniId=${appid}&miniProgramType=${miniProgramType}`
+      } else if (data.linkType === 'link') {
+        url = `detailArticle?path=${encodeURIComponent(data.path)}&id=${id}`
+      }
+
+      if (data.type === 1) {
+        // return `<a href="${url}"><img src="https://t7.baidu.com/it/u=1819248061,230866778&fm=193&f=GIF" alt=""></a>`
+        return `<a href="${url}"><img src="${data.src}" alt=""></a>`
+      } else {
+        return `<a href="${url}">${data.content}</a>`
+      }
+    }
+
     // 做任何你想做的事情
     // 可参考【常用 API】文档，来操作编辑器
-    // alert('hello world')
     Vue.prototype.modal({
       // 弹窗内嵌套组件传递属性
       componentProps: {},
@@ -37,9 +60,17 @@ class LinkMenu extends BtnMenu {
         // fullscreen: false
       },
       // 事件回调
-      methods: {}
+      methods: {
+        click(data) {
+          console.log('data', data)
+          const linkDom = handleLink(data)
+
+          that.editor.cmd.do('insertHTML', linkDom)
+        }
+      }
     })
   }
+
   // 菜单是否被激活（如果不需要，这个函数可以空着）
   // 1. 激活是什么？光标放在一段加粗、下划线的文本时，菜单栏里的 B 和 U 被激活，如下图
   // 2. 什么时候执行这个函数？每次编辑器区域的选区变化（如鼠标操作、键盘操作等），都会触发各个菜单的 tryChangeActive 函数，重新计算菜单的激活状态
