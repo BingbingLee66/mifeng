c35c59eb6cd6dd235104c39cf33694371ef6ad13
xiaoqianggang <xiaoqianggang@9kd.com>
2023-02-07 18:01:17 +0800

    feat：添加 cookie token

--- packages/shanghui-admin/src/utils/token.js
+++ packages/shanghui-admin/src/utils/token.js
@@ -1,11 +1,12 @@
 import { ACCESS_TOKEN } from '@/constant'
+import Cookies from 'js-cookie'
 
 export function getToken() {
-  return localStorage.getItem(ACCESS_TOKEN)
+  return localStorage.getItem(ACCESS_TOKEN) || Cookies.get(ACCESS_TOKEN) || ''
 }
 
 export function setToken(token) {
-  return localStorage.setItem(ACCESS_TOKEN, token)
+  return localStorage.setItem(ACCESS_TOKEN, token) && Cookies.set(ACCESS_TOKEN, token)
 }
 
 export function removeToken() {
