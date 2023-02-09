71c6472c6274bc9e353ed4aac2e49e1ad451ded8
xiaoqianggang <xiaoqianggang@9kd.com>
2023-02-09 15:47:09 +0800

    fix：修复 eslint 错误提示

--- packages/shanghui-admin/.eslintrc.js
+++ packages/shanghui-admin/.eslintrc.js
@@ -13,6 +13,7 @@ module.exports = {
   },
   extends: ['plugin:vue/vue3-essential', '@vue/standard', 'plugin:prettier/recommended'],
   parserOptions: {
+    requireConfigFile: false,
     parser: '@babel/eslint-parser'
   },
   rules: {
