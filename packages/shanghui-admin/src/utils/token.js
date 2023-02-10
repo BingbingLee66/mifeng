dd05e8f37a11648396220c9d82eedca48d17e458
xiaoqianggang <xiaoqianggang@9kd.com>
2023-02-10 12:30:44 +0800

    fix：设置特定域名 cookie

--- packages/shanghui-admin/src/utils/token.js
+++ packages/shanghui-admin/src/utils/token.js
@@ -1,16 +1,16 @@
 import { ACCESS_TOKEN, COOKIE_TOKEN } from '@/constant'
 import Cookies from 'js-cookie'
-
+const { VUE_APP_BASE_URL } = process.env
 export function getToken() {
   return localStorage.getItem(ACCESS_TOKEN) || Cookies.get(COOKIE_TOKEN) || ''
 }
 
 export function setToken(token) {
   localStorage.setItem(ACCESS_TOKEN, token)
-  Cookies.set(COOKIE_TOKEN, token, { domain: '.kaidicloud.com' })
+  Cookies.set(COOKIE_TOKEN, token, { domain: `${VUE_APP_BASE_URL}.kaidicloud.com` })
 }
 
 export function removeToken() {
   localStorage.removeItem(ACCESS_TOKEN)
-  Cookies.remove(COOKIE_TOKEN)
+  Cookies.remove(COOKIE_TOKEN, { domain: `${VUE_APP_BASE_URL}.kaidicloud.com` })
 }
