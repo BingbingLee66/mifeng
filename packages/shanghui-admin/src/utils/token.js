41c353d44b4ca666d91bd2ce081800177e0da5fe
xiaoqianggang <xiaoqianggang@9kd.com>
2023-02-13 10:57:18 +0800

    feat：更改登录存储方式

--- packages/shanghui-admin/src/utils/token.js
+++ packages/shanghui-admin/src/utils/token.js
@@ -1,16 +1,14 @@
-import { ACCESS_TOKEN, COOKIE_TOKEN } from '@/constant'
+import { COOKIE_TOKEN } from '@/constant'
 import Cookies from 'js-cookie'
 
 export function getToken() {
-  return localStorage.getItem(ACCESS_TOKEN) || Cookies.get(COOKIE_TOKEN) || ''
+  return Cookies.get(COOKIE_TOKEN) || ''
 }
 
 export function setToken(token) {
-  localStorage.setItem(ACCESS_TOKEN, token)
   Cookies.set(COOKIE_TOKEN, token, { domain: '.kaidicloud' })
 }
 
 export function removeToken() {
-  localStorage.removeItem(ACCESS_TOKEN)
   Cookies.remove(COOKIE_TOKEN, { domain: '.kaidicloud' })
 }
