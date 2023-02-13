6f6321e9a5a6b31f7bf2a21b83ce12fbd9108a70
xiaoqianggang <xiaoqianggang@9kd.com>
2023-02-13 11:17:09 +0800

    feat：添加环境 cookie

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,15 +1,16 @@
 import Cookies from 'js-cookie'
 
 const TokenKey = 'access-token'
-
+const isDev = process.env.NODE_ENV === 'development'
+const domain = !isDev ? '.kaidicloud' : ''
 export function getToken() {
   return Cookies.get(TokenKey)
 }
 
 export function setToken(token) {
-  return Cookies.set(TokenKey, token, { domain: '.kaidicloud' })
+  return Cookies.set(TokenKey, token, { domain })
 }
 
 export function removeToken() {
-  return Cookies.remove(TokenKey, { domain: '.kaidicloud' })
+  return Cookies.remove(TokenKey, { domain })
 }
