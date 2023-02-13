ce8992e8a1c90f6ce095153c6ea4371712bb9fb1
xiaoqianggang <xiaoqianggang@9kd.com>
2023-02-13 11:08:28 +0800

    fix：修复登录 cookie 设置

--- packages/shanghui-admin/src/utils/token.js
+++ packages/shanghui-admin/src/utils/token.js
@@ -1,14 +1,15 @@
 import { COOKIE_TOKEN } from '@/constant'
 import Cookies from 'js-cookie'
-
+const isProd = process.env.VUE_APP_BUILD_ENV === 'production'
+const domain = isProd ? '.kaidicloud' : ''
 export function getToken() {
   return Cookies.get(COOKIE_TOKEN) || ''
 }
 
 export function setToken(token) {
-  Cookies.set(COOKIE_TOKEN, token, { domain: '.kaidicloud' })
+  Cookies.set(COOKIE_TOKEN, token, { domain })
 }
 
 export function removeToken() {
-  Cookies.remove(COOKIE_TOKEN, { domain: '.kaidicloud' })
+  Cookies.remove(COOKIE_TOKEN, { domain })
 }
