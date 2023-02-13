cca8e5807532e71ab9898756562b35e45954b039
xiaoqianggang <xiaoqianggang@9kd.com>
2023-02-13 11:22:43 +0800

    fix：修复缺失顶级域名

--- packages/shanghui-admin/src/utils/token.js
+++ packages/shanghui-admin/src/utils/token.js
@@ -1,7 +1,7 @@
 import { COOKIE_TOKEN } from '@/constant'
 import Cookies from 'js-cookie'
 const isDev = process.env.NODE_ENV === 'development'
-const domain = !isDev ? '.kaidicloud' : ''
+const domain = !isDev ? '.kaidicloud.com' : ''
 export function getToken() {
   return Cookies.get(COOKIE_TOKEN) || ''
 }
